55b622de768cb059a9cb3e97b76fe7c7996c7f57
Divyansh Harne <divyanshsanjayharne23@gmail.com>
2023-08-19 13:27:05 +0530

    Day 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import './App.css';
+import Header from './components/Header';
+import { ManBat } from './components/Header'; // Destructured way of importing in Node.js
 
-function MyComponent({name, nameforclass, income = 20}){ // Default value can be given here which is used when no value is passed to function.
-  return(   // this fuction can only return a single component, that is why we use empty tag to retyrn more than one component.  
-    <>      
-    <div className={nameforclass}>{name}: ${income}</div>
-    <hr/>
-    </>
-  )
-}
 
 function App() {
   return (
     <div id='23' className='Different'>
+      <Header/>
       Hello 
-      <MyComponent name = {"Dev"}  nameforclass={"div1"} income={200}/>
-      <MyComponent name={"blud"}  nameforclass={"div2"}/>
-      <MyComponent name={"Chatur"}  nameforclass={"div3"} income={310}/> 
+       
       World!!
+
+      <ManBat/>
       <button>Press</button>
     </div>
   );
